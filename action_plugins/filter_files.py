5c6938f44c9b72ed2fd47853c2e75116a0ace29c
Matthew Eager <darthmajo@hotmail.com>
2025-08-28 13:05:53 +0200

    Filter files plugin
    Implemented the filter files plugin.

--- action_plugins/filter_files.py
+++ action_plugins/filter_files.py
@@ -1,12 +1,18 @@
+import re
+
 from ansible.plugins.action import ActionBase
 
 class ActionModule(ActionBase):
     """Takes a list of file matches and filter out the ones we don't want."""
     def run(self, tmp=None, task_vars=None):
         super(ActionModule, self).run(tmp, task_vars)
+        pattern = re.compile("\/proc\/\d+\/status")
+        paths = []
 
         # Loop over all files found and only get the files that we want.
         for file in task_vars.get('found_files')['files']:
-            print(file.get('path'))
+            if re.match(pattern, file.get('path')):
+                paths.append(file.get('path'))
         #print(task_vars['found_files']['files'])
-        return {}
+        return {'paths': paths}
+
